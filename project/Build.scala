dd08aa58389c7759d51ca49df4a8c04d43c84bae
Marc Esquerrà <marc.bayo@picsolve.com>
2013-07-04 19:57:13 +0100

    Project template

---
+++ project/Build.scala
@@ -0,0 +1,30 @@
+import sbt._
+import Keys._
+
+object BuildSettings
+{
+
+	val buildSettings = Defaults.defaultSettings ++ Seq(
+			organization := "w3d.cc",
+			version      := "0.0.1",
+			scalaVersion := "2.10.1",
+			scalacOptions ++= Seq(),
+			libraryDependencies += "com.amazonaws" % "aws-java-sdk" % "1.4.7"
+	)
+
+}
+
+object MyBuild extends Build
+{
+	val projectName = "ScapleDB"
+
+	import BuildSettings._
+
+
+	lazy val root: Project = Project(
+		projectName,
+		file("."),
+		settings = buildSettings
+	)
+
+}
